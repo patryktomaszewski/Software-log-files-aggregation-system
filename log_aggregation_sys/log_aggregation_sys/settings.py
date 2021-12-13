c716eacf1bab64d9d318343678d79b6320517379
patryktomaszewski <patryk.tomaszewski30@gmail.com>
2021-12-13 17:29:25 +0100

    updating settings

--- log_aggregation_sys/log_aggregation_sys/settings.py
+++ log_aggregation_sys/log_aggregation_sys/settings.py
@@ -153,11 +153,21 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+if os.environ.get('GITHUB_WORKFLOW'):
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "test_host"
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = "test_user"
+    EMAIL_HOST_PASSWORD = "test_password"
 
-ALERT_EMAIL_ADDRESS = env("ALERT_EMAIL_ADDRESS")
+    ALERT_EMAIL_ADDRESS = "test_email"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = env("EMAIL_HOST")
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+
+    ALERT_EMAIL_ADDRESS = env("ALERT_EMAIL_ADDRESS")
